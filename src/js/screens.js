5466a4de4bd8cf1ed21e9d6c84322c823a398d48
Sanwed <kozyuckov.alex@gmail.com>
2023-06-09 10:54:36 +0400

    feat: add ability to see three screens at once

--- src/js/screens.js
+++ src/js/screens.js
@@ -1,18 +1,17 @@
-const screensList = document.querySelectorAll('.screen');
-
 const bindScreenButton = (buttonId, screenId) => {
     const button = document.querySelector(`#${buttonId}`);
     const screen = document.querySelector(`#${screenId}`);
 
     button.addEventListener('click', () => {
-        screensList.forEach((screen) => screen.classList.add('hidden'));
-        screen.classList.remove('hidden');
+        button.classList.toggle('screen-button-active')
+        screen.classList.toggle('hidden');
     })
 };
 
 const screens = () => {
     bindScreenButton('tasks-button', 'tasks-screen');
     bindScreenButton('weather-button', 'weather-screen');
+    bindScreenButton('calendar-button', 'calendar-screen');
 }
 
 
